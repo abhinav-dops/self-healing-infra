6376adeaa257fd406398cde2d509cb568f0eadf4
abhinav-dops <abhinavsingh.ag@gmail.com>
2025-12-13 21:51:21 +0530

    input variables for AMI, instance type, VPC, subnet, key, and user-data

---
+++ infra/modules/ec2/main.tf
@@ -0,0 +1,49 @@
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2_sg"
+  vpc_id      = var.vpc_id
+  description = "Allow HTTP and SSH"
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    prefix_list_ids = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+
+  }
+}
+
+resource "aws_key_pair" "main" {
+  key_name   = var.key_name
+  public_key = file(var.public_key_path)
+  #.pem file location
+}
+
+resource "aws_instance" "app.ec2" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  key_name               = aws_key_pair.main.key_name
+
+  user_data = file(var.user_data_path)
+
+  tags = {
+    Name = "self-healing-ec2"
+  }
+}
