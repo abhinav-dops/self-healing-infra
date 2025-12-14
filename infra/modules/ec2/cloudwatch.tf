e6d0e9175413ba5022818b28211db397dc8728e3
abhinav-dops <abhinavsingh.ag@gmail.com>
2025-12-14 18:49:02 +0530

    updated cloudwatch.tf

--- infra/modules/ec2/cloudwatch.tf
+++ infra/modules/ec2/cloudwatch.tf
@@ -2,7 +2,7 @@ resource "aws_cloudwatch_metric_alarm" "high_cpu" {
   alarm_name          = "selfhealing-high-cpu"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = 2
-  metric_name         = "CPUUtilizatoin"
+  metric_name         = "CPUUtilization"
   namespace           = "AWS/EC2"
   period              = 60
   statistic           = "Average"
