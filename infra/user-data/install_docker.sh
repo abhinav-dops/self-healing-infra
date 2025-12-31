58934a56594abbb7c703dae70b6f4b00def1734b
abhinav-dops <abhinavsingh.ag@gmail.com>
2026-01-01 00:06:46 +0530

    updated docker run

--- infra/user-data/install_docker.sh
+++ infra/user-data/install_docker.sh
@@ -2,12 +2,12 @@
 
 sudo dnf update -y
 sudo dnf install docker -y
-sudo service docker start
+sudo systemctl start docker
+sudo systemctl enable docker
 sudo usermod -a -G docker ec2-user
 
 sudo docker pull naqua/self-healing-app:01
 
 sudo docker rm -f selfhealing || true
 
-sudo docker run -d --name selfhealing -p 80:8080 naqua/self-healing-app:01
-
+sudo docker run -d --restart=always --name selfhealing -p 80:8080 naqua/self-healing-app:01
