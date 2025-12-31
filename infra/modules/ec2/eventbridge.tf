961aa0917a984f678ac1bda7555148418045df83
abhinav-dops <abhinavsingh.ag@gmail.com>
2025-12-31 23:25:21 +0530

    fixed arn and fuction_name:

--- infra/modules/ec2/eventbridge.tf
+++ infra/modules/ec2/eventbridge.tf
@@ -15,13 +15,13 @@ resource "aws_cloudwatch_event_rule" "cpu_alarm_state" {
 
 resource "aws_cloudwatch_event_target" "invoke_lambda" {
   rule = aws_cloudwatch_event_rule.cpu_alarm_state.name
-  arn  = aws_lambda_function.selfhealing_ec2_reboot.arn
+  arn  = aws_lambda_function.selfhealing.arn
 }
 
 resource "aws_lambda_permission" "allow_eventbridge" {
   statement_id  = "AllowEventBridgeInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.selfhealing_ec2_reboot.function_name
+  function_name = aws_lambda_function.selfhealing.function_name
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.cpu_alarm_state.arn
 }
